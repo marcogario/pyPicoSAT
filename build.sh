ce57e0bebe20ecbcaa9e4f3cd44bbf46049cf4c7
Marco Gario <marco.gario@gmail.com>
2015-10-24 17:57:49 +0200

    Py3

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-PICOSAT_DIR=$1
-PYTHON=`which python`
+PICOSAT_DIR=$11
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ "X${PICOSAT_DIR}" == "X" ]; then
     echo "Usage: ./build.sh <picosat_dir>"
     exit 1
+fi
+
+if [ "X${PYTHON}" == "X" ]; then
+PYTHON=`which python`
 fi
 
 cd $PICOSAT_DIR
@@ -18,7 +21,7 @@ cd $DIR
 # SWIG
 swig -I${PICOSAT_DIR} -python -o picosat_python_wrap.c picosat_python.i
 # Build
-$PYTHON ./setup.py build
+${PYTHON} ./setup.py build
 
 # PKG (This is done by travis-CI. It is left here for reference)
 # $PYTHON ./setup.py egg_info --tag-date --tag-build=.dev bdist_wheel
